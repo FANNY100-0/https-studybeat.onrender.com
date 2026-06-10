89f8e4c70c22eeddaea2edc5b8861936da17d91a
FANNY100-0 <fannyher801@gmail.com>
2026-06-10 11:01:26 -0600

    Create script.js

---
+++ static/css/static/js/script.js
@@ -0,0 +1,5 @@
+// Funciones básicas para el modo oscuro y navegación
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("StudyBeat cargado correctamente.");
+    // Aquí irían las funciones de animación y modo oscuro
+});
